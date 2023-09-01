b651a8c8db3d0dd7a1ce9ff228998718fa4002ef
Bekah Lane <b.kahlane@gmail.com>
2023-08-31 17:03:06 -0700

    read in multiple yrs of ais to make vessel rasters

---
+++ scratch/vesselrasterizeyears.R
@@ -0,0 +1,83 @@
+library(lubridate)
+library(sf)
+library(terra)
+library(tmap)
+library(tidyverse)
+sf_use_s2(FALSE)
+
+# Create raster template
+study_area <- st_read(here::here("data", "studyarea")) %>% 
+  st_crop(xmin = -122.6501, ymin = 37.5, xmax = -121, ymax = 38) %>% 
+  st_transform("EPSG:3488")
+study_area_template <- rast(ext(study_area), 
+                            resolution = 500,
+                            crs = "EPSG:3488")
+
+create_vessel_raster <- function (vessel_df, season_type, year, template) {
+  # Project lat lon to albers
+  vessel_albers <- vessel_df %>% 
+    st_as_sf(coords = c("LON", "LAT"),
+             crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") %>% 
+    st_transform("EPSG:3488") 
+  # Rasterize points
+  vessel_raster <- vessel_albers %>% 
+    rasterize(template, fun = "count") %>% 
+    crop(study_area, mask = TRUE) 
+  # Classify raster by quantile
+  vessel_quantiles <- quantile(values(vessel_raster), 
+                               c(0.33, 0.67, 1), 
+                               na.rm = TRUE) 
+  # Rename levels
+  vessel_ratings <- classify(vessel_raster, 
+                             c(0, vessel_quantiles),
+                             include.lowest = TRUE)
+  
+  # Rename levels in raster 
+  vessel_levels <- levels(vessel_ratings)[[1]] %>% 
+    transmute(value,
+              Rating = factor(1:3))
+  levels(vessel_ratings) <- vessel_levels
+  
+  # Rename raster layer
+  names(vessel_ratings) <- paste(season_type,collapse="_")
+  
+  # Return a vector
+  as.polygons(vessel_ratings)
+}
+
+for (year in c("2016", "2017", "2018")) {
+  #read in ais rds files by year
+  ais_year <- list.files("outputs/ais", full.names = TRUE, pattern = str_glue("{year}\\.rds"))
+  ais_year_df <- map_df(ais_year, readRDS)
+  #create season column
+  ais_season_year <- ais_year_df %>% 
+    mutate(season = case_when(
+      month(BaseDateTime) <= 3 ~ "Winter",
+      month(BaseDateTime) <= 6 ~ "Spring",
+      month(BaseDateTime) <= 9 ~ "Summer",
+      month(BaseDateTime) <= 12 ~ "Fall"))
+  
+  vessel_density <- ais_season_year %>% 
+    group_by(year(BaseDateTime), season, ReclassifiedVesselType) %>% 
+    group_map(create_vessel_raster, template = study_area_template) %>% 
+    set_names(map_chr(., names))
+  
+  for (i in seq(length(vessel_density))) 
+    names(vessel_density[[i]]) <- "Rating"
+}
+
+
+#WALK 
+walk2(vessel_density, 
+      names(vessel_density), 
+      \(r, n) {
+        year_file <- str_glue("vessel_density_{year}.rds")
+        readRDS(year_file) %>% 
+          extract2(n) %>% 
+          writeVector(file.path("outputs/ais", str_glue("ais_{year}_{n}.shp")), 
+                      overwrite = TRUE))
+      })
+saveRDS(vessel_density, "outputs/vessel_density_{year}.rds")
+}
+
+
